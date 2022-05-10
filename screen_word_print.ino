0603844bdeb88c14a970e8e772d18d66c21ca500
koscott2 <86267888+koscott2@users.noreply.github.com>
2022-05-09 21:08:06 -0500

    Add files via upload

---
+++ screen_word_print.ino
@@ -0,0 +1,83 @@
+
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+// The pins for I2C are defined by the Wire-library. 
+// On an arduino UNO:       A4(SDA), A5(SCL)
+// On an arduino MEGA 2560: 20(SDA), 21(SCL)
+// On an arduino LEONARDO:   2(SDA),  3(SCL), ...
+#define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+#define NUMFLAKES     10 // Number of snowflakes in the animation example
+
+#define LOGO_HEIGHT   16
+#define LOGO_WIDTH    16
+static const unsigned char PROGMEM logo_bmp[] =
+{ 0b00000000, 0b11000000,
+  0b00000001, 0b11000000,
+  0b00000001, 0b11000000,
+  0b00000011, 0b11100000,
+  0b11110011, 0b11100000,
+  0b11111110, 0b11111000,
+  0b01111110, 0b11111111,
+  0b00110011, 0b10011111,
+  0b00011111, 0b11111100,
+  0b00001101, 0b01110000,
+  0b00011011, 0b10100000,
+  0b00111111, 0b11100000,
+  0b00111111, 0b11110000,
+  0b01111100, 0b11110000,
+  0b01110000, 0b01110000,
+  0b00000000, 0b00110000 };
+
+  char incoming[1000];
+
+  
+void setup() {
+  Serial.begin(9600);
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;);
+  }
+  display.clearDisplay();
+//drawrect();
+    drawchar();      
+}
+
+  void loop(){   
+}
+
+
+void drawrect(void) {
+   
+   display.drawRect(0, 0, display.width(), display.height(), WHITE);
+   display.display();
+}
+
+void drawchar(void) {
+  
+  display.setTextSize(2);     
+  display.setTextColor(SSD1306_WHITE); 
+  display.setCursor(0, 0);     
+  display.cp437(true);
+  String CPU = "CPU";
+  String GPU = " GPU";
+  String RAM = " RAM";
+  display.print(CPU);
+  display.setCursor(31, 0);
+  display.print(GPU);
+  display.setCursor(75, 0);        
+  display.println(RAM);
+  display.println("56*");
+
+  display.display();
+  delay(2000);
+}
